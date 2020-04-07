7b946e041d2af237ac66b06c2f3b4855c6dc540c
Zerlings <smzh369@126.com>
2020-04-07 23:53:27 +0800

    1.添加并优化spinner样式属性。2.完善点击事件与重置相关。3.优化xml布局。4.新增动态绑定数据源。

--- app/src/main/java/com/zerlings/smartspinner/MainActivity.kt
+++ app/src/main/java/com/zerlings/smartspinner/MainActivity.kt
@@ -2,6 +2,7 @@ package com.zerlings.smartspinner
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        spinner.setOnItemSelectedListener { view, index ->
+            Toast.makeText(this, spinner.getItemAtPosition(index), Toast.LENGTH_SHORT).show()
+        }
+        spinner.setOnSpinnerResetListener {
+            Toast.makeText(this, spinner.getSelectedItem(), Toast.LENGTH_SHORT).show()
+        }
+        reset.setOnClickListener {
+            val strList = ArrayList<CharSequence>()
+            strList.add("apple")
+            strList.add("apple")
+            strList.add("apple")
+            spinner.setDataSource(strList)
+        }
     }
 }
--- library/src/main/java/com/zerlings/library/SimpleSpinnerAdapter.kt
+++ library/src/main/java/com/zerlings/library/SimpleSpinnerAdapter.kt
@@ -1,34 +1,40 @@
 package com.zerlings.library
 
 import android.support.annotation.ColorInt
+import android.support.annotation.DrawableRes
 import android.support.annotation.LayoutRes
 import android.util.TypedValue
-import kotlinx.android.synthetic.main.item_simple_spinner.view.*
+import kotlinx.android.synthetic.main.spinner_simple_item.view.*
 
-class SimpleSpinnerAdapter(
+class SimpleSpinnerAdapter<T:CharSequence>(
     @LayoutRes layoutResId: Int,
-    stringList: MutableList<CharSequence>,
-    private val itemHeight: Int,
+    stringList: MutableList<T>,
     private val startPadding: Int,
     private val endPadding: Int,
-    @ColorInt private val menuBackground: Int,
+    @DrawableRes private val menuBackground: Int,
     @ColorInt private val textColor: Int,
     private val textSize: Float,
     private val gravity: Int
-) : BaseSpinnerAdapter<CharSequence, BaseViewHolder>(layoutResId, stringList) {
+) : BaseSpinnerAdapter<T, BaseViewHolder>(layoutResId, stringList) {
+
+    private var itemHeight: Int = 1
+
+    internal fun setItemHeight(height: Int){
+        itemHeight = height
+    }
 
     override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
         super.onBindViewHolder(holder, position)
         holder.itemView.apply {
             layoutParams.height = itemHeight
-            ll_simple.setPaddingRelative(startPadding,0,endPadding,0)
-            ll_simple.gravity = gravity
-            ll_simple.setBackgroundColor(menuBackground)
+            tv_simple.setPaddingRelative(startPadding,0,endPadding,0)
+            tv_simple.gravity = gravity
+            tv_simple.setBackgroundResource(menuBackground)
             tv_simple.setTextColor(textColor)
             tv_simple.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize)
             tv_simple.text = dataList[position]
             if (position == selectedPosition){
-                ll_simple.setBackgroundColor(selectedBackground)
+                tv_simple.setBackgroundResource(selectedBackground)
                 tv_simple.setTextColor(selectedColor)
             }
         }
